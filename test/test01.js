a48b50f5912cbfc21e9baa88f4ab5f43897de7e9
xc <Fredric2014@gmail.com>
2014-11-03 14:40:58 +0800

    update

--- test/test01.js
+++ test/test01.js
@@ -4,9 +4,7 @@ var should    	= require("chai").should();
 describe('Array', function(){
   describe('#indexOf01()', function(){
     it('should return -1 when the value is not present', function(){
-      //assert.equal(-1, [1,2,3].indexOf(5));
-      //assert.equal(-1, [1,2,3].indexOf(0));
-      
+     
       ([1,2,3].indexOf(5)).should.equal(-1);
       ([1,2,3].indexOf(0)).should.equal(-1);
     })
